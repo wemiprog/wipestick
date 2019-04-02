834012820ce6794547948a043fca808dfdfb1782
Michael Wepf <mwepf1@gmail.com>
2019-04-02 17:55:16 +0200

    Added service file

--- main/airootfs/root/customize_airootfs.sh
+++ main/airootfs/root/customize_airootfs.sh
@@ -2,7 +2,7 @@
 
 set -e -u
 
-sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(de_CH\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
@@ -21,3 +21,25 @@ sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
+
+# Create service file
+# - Autostarts the skript wipe-sda
+# - Enables in- and output on tty2
+cat << EOF >> /etc/systemd/system/wipe-sda.service
+[Unit]
+Description=Wipe /dev/sda after user clicked OK
+After=getty@tty2.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/wipe-sda
+StandardInput=tty
+TTYPath=/dev/tty2
+TTYReset=yes
+TTYVHangup=yes
+
+[Install]
+WantedBy=default.target
+EOF
+
+

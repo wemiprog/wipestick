d2e4a6ef20c24d1130cb657abc8395958789ddbd
Michael Wepf <mwepf1@gmail.com>
2019-04-03 11:21:31 +0200

    Set to verbose

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ chmod +x airootfs/root/customize_airootfs.sh
 chmod +x airootfs/root/.automated_script.sh
 chmod +x airootfs/etc/systemd/scripts/choose-mirror
 
-./build.sh
+./build.sh -v
 cp out/*.iso ../

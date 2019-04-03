8a46b821e309c8d33abce8781b5cf01c06d058ee
Michael Wepf <mwepf1@gmail.com>
2019-04-03 08:52:16 +0200

    Fixed exec permissions

--- start.sh
+++ start.sh
@@ -2,5 +2,9 @@
 
 cd main
 chmod +x build.sh
+chmod +x airootfs/root/customize_airootfs.sh
+chmod +x airootfs/root/.automated_script.sh
+chmod +x airootfs/etc/systemd/scripts/choose-mirror
+
 ./build.sh
 cp out/*.iso ../
